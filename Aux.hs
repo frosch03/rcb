6577adaa6edb1a51f9042649bdba54a780704803
Matthias Brettschneider <frosch03@frosch03.de>
2019-04-02 18:08:06 +0200

    adding two rss read functions to the bot
    - fefe - return last posting of fefe
    - xkcd - return last posting of xkcd
    - fefe n - return n postings of fefe
    - xkcd n - return n postings of xkcd

--- Aux.hs
+++ Aux.hs
@@ -5,6 +5,11 @@ import Data.Char (ord)
 import Crypto.Hash.SHA256 (hash)
 import qualified Data.ByteString as BS (unpack, pack)
 import Text.Printf (printf)
+import Data.Time.Clock (diffTimeToPicoseconds, utctDayTime, getCurrentTime)
+
+
+secondsOfTheDay :: IO Int
+secondsOfTheDay = getCurrentTime >>= (\x -> return $ fromInteger . (flip div $ 1000000000000) . diffTimeToPicoseconds . utctDayTime $ x)
 
 enclose :: String -> Char -> String
 enclose s c = (c:s) ++ [c]
--- RCT.hs
+++ RCT.hs
@@ -1,16 +1,20 @@
 module RCT where
 
+import Aux (secondsOfTheDay)
 import Ascii
 import Message
 import Method (RoomId)
 import Configuration
-import Plugin.RssReader.Reader (readFeed, title)
+import Plugin.RssReader.Reader (readFeed, title, link, description)
 
 import FRP.Yampa
 import Data.Text (Text, pack, unpack)
+import Text.Read (readMaybe)
 import Network.WebSockets (ClientApp, receiveData, sendClose, sendTextData)
 import Network.WebSockets.Connection (Connection(..)) 
 
+import Text.ParserCombinators.Parsec
+import Text.Parsec.Perm
 
 sendAndShow :: Connection -> Message -> IO ()
 sendAndShow c s = do
@@ -50,4 +54,52 @@ actuate c _ (Just (rid, s)) = do
     otherwise -> putStrLn s >> return False
 
 
+
+grepImgUrl :: String -> String
+grepImgUrl s = fst $
+    case (parse pImageTag' "" s) of
+      Left err  -> error $ show err
+      Right xs  -> xs
+    where 
+      pImageTag' :: GenParser Char st (String, String)
+      pImageTag' = do
+        many $ noneOf "<"
+        string "<img src=\""
+        src <- manyTill anyChar (try $ string "\" title")
+        string "=\""
+        title <- manyTill anyChar (try $ string "\" alt")
+        string "=\""
+        alt <- manyTill anyChar (try $ string "\" />")
+        many anyChar
+        return (src, title)
+
+lastNxkcd :: Connection -> Method.RoomId -> String -> IO ()
+lastNxkcd c rid s = do
+  let n = if (length . tail . words $ s) > 0
+          then maybe 1 id (readMaybe (head . tail . words $ s) :: Maybe Int)
+          else 1
+  xs <- readFeed n "https://www.xkcd.com/rss.xml"
+  let ts = map title       xs
+      ls = map link        xs
+      ds = map description xs
+      ms = zipWith3 (\d t l -> "[" ++ t ++ "](" ++ l ++ "): " ++ (grepImgUrl d)) ds ts ls
+  mid <- secondsOfTheDay
+  mapM (\m -> putStrLn m) ms
+  mapM (\m -> sendTextData c . pack . ascii $ mkSendMsg mid rid m) ms
+  return ()
+
+
+lastNfefe :: Connection -> Method.RoomId -> String -> IO ()
+lastNfefe c rid s = do
+  let n = if (length . tail . words $ s) > 0
+          then maybe 1 id (readMaybe (head . tail . words $ s) :: Maybe Int)
+          else 1
+  xs <- readFeed n "http://blog.fefe.de/rss.xml?html"
+  let ts = map title xs
+      ls = map link  xs
+      ms = zipWith (\t l -> "[" ++ t ++ "](" ++ l ++ ")") ts ls
+  mid <- secondsOfTheDay
+  mapM (\m -> putStrLn m) ms
+  mapM (\m -> sendTextData c . pack . ascii $ mkSendMsg mid rid m) ms
+  return ()
 
