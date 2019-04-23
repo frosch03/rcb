b601086acafdfedd7ee7e85e22096726b3e12c4a
Matthias Brettschneider <frosch03@frosch03.de>
2019-04-24 00:07:18 +0200

    adding multithreads
    - one thread per
      - rsscommands
      - rsspush
    - Communication via MVars

--- Main.hs
+++ Main.hs
@@ -2,10 +2,13 @@ module Main where
 
 import Configuration
 
+import Plugin.RSS.Commands
+
 import RssCommands as RC
 import RssPush     as RP
 
 import Control.Concurrent (forkIO)
+import Control.Concurrent.MVar
 import Control.Monad (forever, void)
 import Network.WebSockets (ClientApp)
 import Wuss (runSecureClient)
@@ -15,12 +18,14 @@ import FRP.Yampa (reactimate)
 
 reactiveWS :: ClientApp ()
 reactiveWS c = do
+  config <- newMVar rssConfig
+  putStrLn "DISABLED: >>> Starting Continuous News Deliverer"
   putStrLn "Starting Continuous News Deliverer"
   void . forkIO . forever $ do
-      reactimate (RP.initialize c) (RP.sense c) (RP.actuate c) RP.process
+      reactimate (RP.initialize config c) (RP.sense config c) (RP.actuate c) RP.process
 
   putStrLn "Starting Rss Commands"
-  reactimate (RC.initialize c) (RC.sense c) (RC.actuate c) RC.process
+  reactimate (RC.initialize c) (RC.sense c) (RC.actuate config c) RC.process
 
 main :: IO ()
 main = runSecureClient rct_server rct_port rct_path reactiveWS
