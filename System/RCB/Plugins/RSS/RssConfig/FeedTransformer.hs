456859e89cf462711e37473cf47a7a11d4bcfac8
Matthias Brettschneider <frosch03@frosch03.de>
2019-05-02 02:09:14 +0200

    adding FeedTrasnformer with transformable instance

---
+++ System/RCB/Plugins/RSS/RssConfig/FeedTransformer.hs
@@ -0,0 +1,21 @@
+-------------------------------------------------------------------------------
+-- | Module      :  System.RCB.Plugins.RSS.RssConfig.FeedDescriptor
+--   Copyright   :  (c) Matthias Brettschneider 2019
+--   License     :  as-is
+--
+--   Maintainer  :  frosch03@gmail.com
+--   Stability   :  unstable
+--   Portability :  unportable
+--
+-------------------------------------------------------------------------------
+
+module System.RCB.Plugins.RSS.RssConfig.FeedTransformer
+    ( FeedTransformer(..)
+    , TTitle(..)
+    , TLink(..)
+    , TDescription(..)
+    )
+where
+
+import System.RCB.Plugins.RSS.RssConfig.FeedTransformer.Datatype
+import System.RCB.Plugins.RSS.RssConfig.FeedTransformer.Instances
---
+++ System/RCB/Plugins/RSS/RssConfig/FeedTransformer/Datatype.hs
@@ -0,0 +1,35 @@
+-------------------------------------------------------------------------------
+-- | Module      :  System.RCB.Plugins.RSS.RssConfig.FeedTransformer.Datatype
+--   Copyright   :  (c) Matthias Brettschneider 2019
+--   License     :  as-is
+--
+--   Maintainer  :  frosch03@gmail.com
+--   Stability   :  unstable
+--   Portability :  unportable
+--
+-------------------------------------------------------------------------------
+
+module System.RCB.Plugins.RSS.RssConfig.FeedTransformer.Datatype
+where
+
+data TTitle
+    = Tempty
+    | Tdrop_quotes
+    deriving (Show, Read)
+
+data TLink
+    = Lempty
+    deriving (Show, Read)
+
+data TDescription
+    = Dempty
+    | Dimgurl
+    deriving (Show, Read)
+
+data FeedTransformer
+    = FeedTransformer
+      { feedTransformer_title :: Maybe TTitle
+      , feedTransformer_link :: Maybe TLink
+      , feedTransformer_description :: Maybe TDescription
+      }
+    deriving (Show, Read)
